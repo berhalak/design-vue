5b11c644dda136bb4374e3bc0d7df5eb526a1047
Jarosław Sadziński <jaroslaw.sadzinski@gmail.com>
2021-08-09 01:16:44 +0200

    Init

--- src/components/Market.vue
+++ src/components/Market.vue
@@ -1,3 +1,59 @@
 <template>
-  <hero-switch />
+	<div class="bg-gray-200">
+		<div class="container mx-auto max-w-4xl">
+			<div class="py-10 flex items-center">
+				<div class="pr-10">Logo</div>
+				<div class="w-80">
+					<div class="shadow rounded-2xl p-2 bg-white">
+						<hero name="search" class="h-4 text-yellow-600" />
+					</div>
+				</div>
+				<div
+					class="
+						flex
+						justify-between
+						w-80
+						ml-10
+						font-bold
+						text-gray-600
+					"
+				>
+					<div class="hover:text-blue-800 cursor-pointer">Home</div>
+					<div class="hover:text-blue-800 cursor-pointer">
+						Documentation
+					</div>
+					<div class="hover:text-blue-800 cursor-pointer">
+						Community
+					</div>
+				</div>
+				<div class="ml-auto font-bold text-gray-600">menu 2</div>
+			</div>
+			<div class="py-10">
+				<div
+					class="text-5xl font-semibold text-blue-800 tracking-tight"
+				>
+					Marketplace
+				</div>
+				<div class="text-xl tracking-tight w-72 mt-4 text-gray-500">
+					Discover a list of Applications built on the platform.
+				</div>
+			</div>
+		</div>
+
+		<div class="bg-white pt-8">
+			<div class="container mx-auto max-w-4xl">
+				<div class="flex flex-col gap-1">
+					<div class="py-2 text-blue-800 text-xl tracking-tight font-semibold">
+						Industries
+					</div>
+					<div class="text-gray-600 font-semibold">Airlines</div>
+					<div class="text-gray-600 font-semibold">Banking</div>
+					<div class="text-gray-600 font-semibold">Industries</div>
+					<div class="text-gray-600 font-semibold">Capital Markets</div>
+					<div class="text-gray-600 font-semibold">Industries</div>
+				</div>
+				<div></div>
+			</div>
+		</div>
+	</div>
 </template>
